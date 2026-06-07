7d6bb99abc5644507ef1b2741115d662d39d446b
aya <ourizaaya731@email.com>
2026-06-07 19:59:56 +0100

    Mise à jour de la page Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -6,6 +6,7 @@ import { RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product';
 import { CartService } from '../../services/cart';
 import { Product } from '../../models/product.model';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -68,4 +69,14 @@ export class Home implements OnInit {
   getStars(rating: number): number[] {
     return Array(Math.floor(rating)).fill(0);
   }
+  @ViewChild('categoriesTrack') categoriesTrack!: ElementRef;
+
+scrollCategories(direction: 'left' | 'right'): void {
+  const track = this.categoriesTrack.nativeElement;
+  const scrollAmount = 300;
+  track.scrollBy({
+    left: direction === 'right' ? scrollAmount : -scrollAmount,
+    behavior: 'smooth'
+  });
+}
 }
